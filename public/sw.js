31724e2cdb56e3cf6d76ddfa52a48921870d9e12
Social Boost horizon <exaucenapopolo2@gmail.com>
2025-12-17 20:05:05 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'sbpwa-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/offline.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      }).catch(() => caches.match('/offline.html'));
+    })
+  );
+});
